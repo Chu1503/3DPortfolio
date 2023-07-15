0060515e03fc51c512987daa5ecd6dbd0da81f8f
Shiv Akash <shivakash2003@gmail.com>
2023-07-16 00:47:53 +0530

    additional media queries

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,15 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-import "./Contact.css"; // Import the CSS file
+import "./Contact.css";
 
 const Contact = () => {
   const formRef = useRef();
@@ -18,6 +20,7 @@ const Contact = () => {
   });
 
   const [loading, setLoading] = useState(false);
+  const isDesktop = window.innerWidth > 768; // Adjust the breakpoint as needed
 
   const handleChange = (e) => {
     const { target } = e;
@@ -35,8 +38,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_chu1503",
+        "template_90dohpu",
         {
           from_name: form.name,
           to_name: "Shiv Akash",
@@ -44,26 +47,32 @@ const Contact = () => {
           to_email: "shivakash2003@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "W0mg14nvrpmrC4pU5"
       )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+      .then(() => {
+        setLoading(false);
+        toast.success(
+          "Thank you. I will get back to you as soon as possible!",
+          {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 5000,
+          }
+        );
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error("EmailJS Error:", error);
+        toast.error("Ahh, something went wrong. Please try again.", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          autoClose: 5000,
+        });
+      });
   };
 
   return (
@@ -89,8 +98,9 @@ const Contact = () => {
               name="name"
               value={form.name}
               onChange={handleChange}
-              placeholder="Enter your name"
+              placeholder="Enter your name *"
               className="bg-[#020302] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -100,8 +110,9 @@ const Contact = () => {
               name="email"
               value={form.email}
               onChange={handleChange}
-              placeholder="Enter your email"
+              placeholder="Enter your email *"
               className="bg-[#020302] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -113,6 +124,7 @@ const Contact = () => {
               onChange={handleChange}
               placeholder="Type anything here..."
               className="bg-[#020302] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
 
@@ -132,12 +144,25 @@ const Contact = () => {
         <EarthCanvas />
       </motion.div>
 
-      <motion.img
-        variants={slideIn("right", "tween", 0.2, 1)}
-        src="https://cdn.discordapp.com/attachments/894801439992475768/1129756255167197314/logo.png"
-        alt="Mobile view image"
-        className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px] contact-image`}
-      />
+      {isDesktop ? null : (
+        <motion.div
+          variants={slideIn("right", "tween", 0.2, 1)}
+          className="xl:flex-1 flex justify-center items-center xl:h-auto md:h-[550px] h-[350px] contact-image"
+        >
+          <div
+            className="rounded-full overflow-hidden w-[300px] h-[300px]"
+            style={{ maxWidth: "100%" }}
+          >
+            <img
+              src="https://cdn.discordapp.com/attachments/894801439992475768/1129842946401894420/mobile_world.png"
+              alt="Mobile view image"
+              style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </div>
+        </motion.div>
+      )}
+
+      <ToastContainer />
     </div>
   );
 };
